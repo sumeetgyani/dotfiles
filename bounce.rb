edae4813efc5b6f641e732ec920648b41f0d81ce
sumeetgyani <sumeetkgyani@gmail.com>
2017-02-09 14:24:24 -0800

    Streamline script

--- bounce.rb
+++ bounce.rb
@@ -1,25 +1,32 @@
 #!/usr/bin/ruby
+
 # Get all running containers
 running_containers = `docker inspect --format='{{.Name}}' $(sudo docker ps -q --no-trunc)`.lines
 # Removing leading slash in name because docker restart cannot except that format
 running_containers.map! {|container| container[1..-1]}
 
-begin
-	puts "\n0) Display running scripts"
-	running_containers.each_with_index do |container, index|
-		puts (index+1).to_s + ') ' + container
-	end
-	user_choice = $stdin.gets.chomp.to_i
-	if user_choice === 0
-		puts "\nRunnings scripts:"
-		puts "================="
-		# Only show processes that start with "php" or "hhvm"
-		# TODO figure out why the or operator doesn't work with grep
-		puts `ps -e -o command | grep "^php "`
-		puts `ps -e -o command | grep "^hhvm "`
-	else
-		chosen_container = running_containers[user_choice-1]
-		puts "Restarting #{chosen_container}"
-		puts `docker restart #{chosen_container}`
-	end
-end until user_choice != 0
+puts "\nRunnings scripts:"
+puts "================="
+# Only show processes that start with "php" or "hhvm"
+# TODO figure out why the or operator doesn't work with grep
+puts `ps -e -o command | grep "^php "`
+puts `ps -e -o command | grep "^hhvm "`
+running_containers.each_with_index do |container, index|
+	puts (index+1).to_s + ') ' + container
+end
+puts 'q) quit'
+
+def isValid(user_choice, optionsLength)
+	return user_choice == 'q' || (user_choice.to_i > 0 && user_choice.to_i <= optionsLength)
+end
+
+user_choice = $stdin.gets.chomp
+while !isValid(user_choice, running_containers.length)
+	puts "Invalid input. Please select one of the numbers or 'q'"
+	user_choice = $stdin.gets.chomp
+end
+
+return if user_choice == 'q'
+chosen_container = running_containers[user_choice.to_i-1]
+puts "Restarting #{chosen_container}"
+puts `docker restart #{chosen_container}`
