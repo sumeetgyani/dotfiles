24516cebfc08ed06be4d036de27a92a160a73224
sumeetgyani <sumeetkgyani@gmail.com>
2017-02-11 16:43:11 -0800

    Update zsh setup

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p ~/.zsh/zsh-autosuggestions;
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions;
 ln -sf ~/dotfiles/zshrc ~/.zshrc;
-ln -sf ~/dotfiles/zsh ~/.zsh;
