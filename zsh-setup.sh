eec98ed5057466c9b8da85acdc8b3a1c8cdde5ba
Mahone <sumeetkgyani@gmail.com>
2020-03-15 15:38:15 -0400

    Remove old files

--- zsh-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.zsh/zsh-autosuggestions;
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions;
-ln -sf ~/dotfiles/zshrc ~/.zshrc;
-npm install --global pure-prompt;
